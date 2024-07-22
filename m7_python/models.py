025ba21c0b847ae80dfc06c27755867f489e9db3
Peter <mendez.pedro94@gmail.com>
2024-07-21 22:35:36 -0400

    add models

--- m7_python/models.py
+++ m7_python/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Usuario(models.Model):
+    user = models.OneToOneField(User, on_delete=models.DO_NOTHING)
+    rut = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=200)
+    telefono_personal = models.IntegerField()
+    correo_electronico = models.EmailField()
+    TIPO_USUARIO = {
+        'arrendatario':'arrendatario',
+        'arrendador':'arrendador'
+    }
+    tipo_usuario = models.CharField(max_length=12, choices=TIPO_USUARIO)
+
+
+class Comuna(models.Model):
+    nombre_comuna = models.CharField(max_length=50)
+    region = models.CharField(max_length=50)
+
+
+class Inmueble(models.Model):
+    propietario = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
+    nombre = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=250)
+    m2_construidos = models.FloatField()
+    m2_totales = models.FloatField()
+    cantidad_estacionamientos = models.IntegerField()
+    cantidad_habitaciones = models.IntegerField()
+    cantidad_banos = models.IntegerField()
+    direccion = models.CharField(max_length=50)
+    comuna = models.ForeignKey(Comuna, on_delete=models.DO_NOTHING)
+    TIPO_INMUEBLE = {
+        'casa':'casa',
+        'departamento':'departamento',
+        'parcela':'parcela'
+    }
+    tipo_inmueble = models.CharField(choices=TIPO_INMUEBLE, max_length=20)
+    precio_arriendo = models.IntegerField()
+    disponible = models.BooleanField(default=True)
+    solicitudes = models.JSONField(blank=True)
+    arrendatario = models.OneToOneField(Usuario, blank=True, on_delete=models.DO_NOTHING, related_name='arrendatario')
+
