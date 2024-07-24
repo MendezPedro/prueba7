eddb12d50b46c859a660eedd6a3ac00bb55dda07
Peter <mendez.pedro94@gmail.com>
2024-07-23 22:27:16 -0400

    add admin url

--- m7_python/admin.py
+++ m7_python/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from m7_python.models import Comuna, Usuario, Inmueble
 
 # Register your models here.
+
+admin.site.register(Comuna)
+admin.site.register(Usuario)
+admin.site.register(Inmueble)
--- m7_python/urls.py
+++ m7_python/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
