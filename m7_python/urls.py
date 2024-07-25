064aa56f35c6ab264bc46220a5fd15bbc5d5a210
Peter <mendez.pedro94@gmail.com>
2024-07-24 21:43:34 -0400

    templates hito 3

--- m7_python/urls.py
+++ m7_python/urls.py
@@ -3,6 +3,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.index, name='index'),
+    path('crear_user', views.crear_user, name='crear_user'),
+    path('perfil', views.actualizar_usuario, name='perfil'),
+
     path('accounts/', include('django.contrib.auth.urls')),
 ]
