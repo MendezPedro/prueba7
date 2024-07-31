6502158d6cc251a897d10f807a6e7ef1d25da9a7
Peter <mendez.pedro94@gmail.com>
2024-07-30 23:00:44 -0400

    uodate url

--- m7_python/urls.py
+++ m7_python/urls.py
@@ -4,8 +4,14 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('crear_user', views.crear_user, name='crear_user'),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('perfil', views.actualizar_usuario, name='perfil'),
 
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('crear_user', views.crear_user, name='crear_user'),
+    path('crear_inmueble', views.crear_inmueble, name='crear_inmueble'),
+    path('editar_inmueble', views.editar_inmueble, name='editar_inmueble'),
+    path('listar_inmueble/<id>', views.listar_inmueble, name='listar_inmueble'),
+    path('listar', views.listar, name='listar'),
+    path('inmueble_comuna', views.inmueble_comuna, name='inmueble_comuna'),
+    
 ]
